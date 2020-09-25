84f9af9e871b6a6e5a63e28268dd3b2f7ebc28a4
旧人 <wx_c678f0d01fe84d319260914830b0c27e@git.code.tencent.com>
2020-09-25 14:44:41 +0800

    2020.9.25 14.44 国赐  地图更新

--- miniprogram-driver/pages/home/home.js
+++ miniprogram-driver/pages/home/home.js
@@ -1,68 +1,30 @@
 Page({
+ 
+  /**
+   * 页面的初始数据
+   */
   data: {
-  menuList: [{
-   name: "快车"
-  }, {
-   name: "顺风车"
-  }, {
-   name: "外卖"
-  }, {
-   name: "单车"
-  }, {
-   name: "礼橙专车"
-  }, {
-   name: "出租车"
-  }, {
-   name: "公交"
-  }, {
-   name: "代驾"
-  }, {
-   name: "豪华车"
-  }, {
-   name: "自驾租车"
-  }, {
-   name: "拼车"
-  }, {
-   name: "二手车"
-  }],
-  tabScroll: 0,
-  currentTab: 0,
-  windowHeight: '',
-  windowWidth: ''
+    currentIndex: 0,
+   
   },
-  onLoad: function() { 
-  wx.getSystemInfo({  // 获取当前设备的宽高，文档有
-   success: (res) => { 
-   this.setData({
-    windowHeight: res.windowHeight,
-    windowWidth: res.windowWidth
-   })
-   },
-  })
+ 
+  //swiper切换时会调用
+  pagechange: function (e) {
+    if ("touch" === e.detail.source) {
+      let currentPageIndex = this.data.currentIndex
+      currentPageIndex = (currentPageIndex + 1) % 3
+      this.setData({
+        currentIndex: currentPageIndex
+      })
+    }
   },
-  onReady: function() {
-  wx.setNavigationBarTitle({ //修改标题文字
-   title: ''
-  })
-  },
-  clickMenu: function(e) {
-  var current = e.currentTarget.dataset.current //获取当前tab的index
-  var tabWidth = this.data.windowWidth / 5 // 导航tab共5个，获取一个的宽度
-  this.setData({
-   tabScroll: (current - 2) * tabWidth //使点击的tab始终在居中位置
-  }) 
-  if (this.data.currentTab == current) {
-   return false
-  } else {
-   this.setData({currentTab: current })
+  //用户点击tab时调用
+  titleClick: function (e) {
+    let currentPageIndex =
+      this.setData({
+        //拿到当前索引并动态改变
+        currentIndex: e.currentTarget.dataset.idx
+      })
   }
-  },
-  changeContent: function(e) {
-  var current = e.detail.current // 获取当前内容所在index,文档有
-  var tabWidth = this.data.windowWidth / 5 
-  this.setData({
-   currentTab: current,
-   tabScroll: (current - 2) * tabWidth
-  })
-  }
- })
+ 
+})
