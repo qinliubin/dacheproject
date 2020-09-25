b371c3ef5065151ece7b6bffe9cd14ffb37be534
旧人 <wx_c678f0d01fe84d319260914830b0c27e@git.code.tencent.com>
2020-09-25 23:38:59 +0800

    2020.9.25 23.38  林杰  更新司机端资料填写 阻止没登录号

--- miniprogram-driver/pages/home/home.js
+++ miniprogram-driver/pages/home/home.js
@@ -13,28 +13,35 @@ Page({
   },
   onLoad: function () {
     var value = wx.getStorageSync('FAUserAcc');
-    var that = this;
-    wx.request({
-      // url: 'http://49.234.64.182:86/index.php/applet/FALogin/onLoadphone',
-      url: 'http://tp6.com/index.php/applet/FALogin/onLoadphone', 
-      data: {
-        phone:value
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success (res) {
-        if (res.data.code==1001) {
-          that.setData({
-            modalmask:true
-          })
-        } else {
-          that.setData({
-            modalmask:false
-          })
+    //判断是否登录
+    if (value=='') {
+      wx.navigateTo({
+        url: '/pages/register/register',
+      })
+    } else {
+      var that = this;
+      wx.request({
+        // url: 'http://49.234.64.182:86/index.php/applet/FALogin/onLoadphone',
+        url: 'http://tp6.com/index.php/applet/FALogin/onLoadphone', 
+        data: {
+          phone:value
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success (res) {
+          if (res.data.code==1001) {
+            that.setData({
+              modalmask:true
+            })
+          } else {
+            that.setData({
+              modalmask:false
+            })
+          }
         }
-      }
-    })
+      })
+    }
   },
   IDnum: function (e) {
     this.setData({
@@ -51,7 +58,9 @@ Page({
       driving: e.detail.value
     })
   },
+  //资料提交
   insubmit:function(e){
+    var that=this;
     if(this.data.IDnum.length == 0||this.data.drive.length == 0||this.data.driving.length == 0){
       wx.showToast({
         title: '请填写好资料！',
@@ -63,6 +72,7 @@ Page({
         // url: 'http://49.234.64.182:86/index.php/applet/FALogin/modifiedData',
         url: 'http://tp6.com/index.php/applet/FALogin/modifiedData', 
         data: {
+          phone:wx.getStorageSync('FAUserAcc'),
           IDnum:this.data.IDnum,
           drive:this.data.drive,
           driving:this.data.driving,
@@ -72,13 +82,34 @@ Page({
         },
         success (res) {
           if (res.data.code==1001) {
-            
-          } else {
-            
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'success',
+              duration: 1500,
+              success:()=>{
+                setTimeout(()=> {
+                  that.setData({
+                    modalmask:false
+                  })
+                },1500)
+              }
+            })
           }
         }
       })
     )
+  },
+//个人中心
+  onPersonalCenter:function(){
+    wx.navigateTo({
+      url: '/pages/PersonalCenter/PersonalCenter',
+    })
+  },
+  //活动中心
+  onActivityCenter:function(){
+    wx.navigateTo({
+      url: '/pages/activityCenter/activityCenter',
+    })
   },
   //swiper切换时会调用
   pagechange: function (e) {
